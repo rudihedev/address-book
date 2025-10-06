5c95b599a62b8691342635d39a41eba51320b315
rudihedev <rudihedev@gmail.com>
2025-10-06 13:16:26 +0700

    add index.js

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# address
+# Address Book
 
 This project is dealing with organize information on name, phone, email, and address of every single person. It was developed based on pure HTML, CSS, and JavaScript. It demonstrates in how all these three components can work together to create a useful information.
 
--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("My Address Book Log");
+console.log("Address Book");
+
+const contacts = [
+  {
+    id: 1,
+    fullName: "Rudi Heriansyah",
+    phone: "+62-811-9870-2232",
+    email: "rudihedev@gmail.com",
+    address: "Palembang, Indonesia",
+  },
+  {
+    id: 2,
+    fullName: "Irwanto Emillio",
+    phone: "+60-232-2329-1122",
+    email: "emillio@gmail.com",
+    address: "Johor, Malaysia",
+  },
+  {
+    id: 3,
+    fullName: "Devita Agustina",
+    phone: "+966-111-3342-1316",
+    email: "ita@gmail.com",
+    address: "Makkah, Saudi Arabia",
+  },
+];
+
+console.log(contacts);

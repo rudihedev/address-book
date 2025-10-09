9d6b7d159088943dd5f1f4de832ba027ab1506f4
rudihedev <rudihedev@gmail.com>
2025-10-09 21:48:30 +0700

    add function in the script

--- index.js
+++ index.js
@@ -1,27 +1,56 @@
 console.log("Address Book");
 
-const contacts = [
+const dataContacts = [
   {
     id: 1,
     fullName: "Rudi Heriansyah",
     phone: "+62-811-9870-2232",
-    email: "rudiheku@gmail.com",
-    address: "Palembang, Indonesia",
+    email: "rudihe@gmail.com",
+    address: {
+      street: "Jln. Sudirman, No. 1",
+      city: "Palembang",
+      zipCode: "30178",
+      country: "Indonesia",
+    },
   },
   {
     id: 2,
     fullName: "Irwanto Emillio",
     phone: "+60-232-2329-1122",
     email: "emillio@gmail.com",
-    address: "Johor, Malaysia",
+    address: {
+      street: "Jln. Batai 7, No. 5",
+      city: "Johor",
+      zipCode: "24232",
+      country: "Malaysia",
+    },
   },
   {
     id: 3,
     fullName: "Devita Agustina",
     phone: "+966-111-3342-1316",
     email: "ita@gmail.com",
-    address: "Makkah, Saudi Arabia",
+    address: {
+      street: "Utsman bin Affan St., No. 8",
+      city: "Makkah",
+      zipCode: "90056",
+      country: "Saudi Arabia",
+    },
   },
 ];
 
-console.log(contacts);
+function viewContacts(contacts) {
+  for (let index = 0; index < contacts.length; index++) {
+    const contact = contacts[index];
+    console.log(`
+    👤 ${contact.fullName}
+    📞 ${contact.phone} 
+    ✉️ ${contact.email} 
+    🏠 ${contact.address.street} 
+    🏙️ ${contact.address.city} ${contact.address.zipCode} 
+    🌍 ${contact.address.country}
+  `);
+  }
+}
+
+viewContacts(dataContacts);
